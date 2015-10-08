15fdf383dc29b43b7c9228d35b6901bd347402af
Julien00859 <Julien00859@gmail.com>
2015-10-08 21:26:26 +0200

    Ajout de la fonction write

--- js/LiveCoding.js
+++ js/LiveCoding.js
@@ -1,13 +1,41 @@
 function main() {
     document.getElementById("curseur").style.borderColor = "cornflowerblue";
     document.getElementsByTagName("main")[0].style.backgroundColor = "rgb(40, 44, 52)";
-    setInterval(cursor, 500);
+    setInterval(blinkCursor, 500);
 }
 
-function cursor() {
-    if (document.getElementById("curseur").style.borderColor == "cornflowerblue") {
-        document.getElementById("curseur").style.borderColor = document.getElementsByTagName("main")[0].style.backgroundColor;
-    } else {
-        document.getElementById("curseur").style.borderColor = "cornflowerblue";
+function blinkCursor() {
+    if (document.getElementById("curseur")) {
+        if (document.getElementById("curseur").style.borderColor == "cornflowerblue") {
+            document.getElementById("curseur").style.borderColor = document.getElementsByTagName("main")[0].style.backgroundColor;
+        } else {
+            document.getElementById("curseur").style.borderColor = "cornflowerblue";
+        }
     }
+}
+
+function write(text, ypos, xpos) {
+    var lines = document.getElementById("code").getElementsByTagName("code");
+    var line = lines[ypos].textContent;
+    if (text.indexOf("\n") == -1) {
+        lines[ypos].textContent = line.substring(0, xpos) + text + line.substring(xpos, line.length);
+    } else {
+        var here = document.getElementById("code").getElementsByTagName("p")[ypos+1];
+        line = line.substring(0, xpos) + text + line.substring(xpos, line.length);
+        line = line.split("\n");
+        lines[ypos].textContent = line[0];
+        for (var n=1; n < line.length; n++) {
+            var p = document.createElement("p");
+            p.innerHTML = "<p><span class=line>" + (ypos + n) +"</span><code>" + line[n] + "</code></p>";
+            document.getElementById("code").insertBefore(p, here);
+        }
+    }
+    for (var n=ypos, lines=document.getElementById("code").getElementsByClassName("line"); n<lines.length; n++) {
+        lines[n].textContent = n+1;
+    }
+}
+
+function cursor(xpos, ypos) {
+    var line = document.getElementById("code").getElementsByTagName("code")[ypos].textContent;
+
 }
