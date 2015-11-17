e3cab27c52731b2365f56f201a94e758b48d19ae
Julien00859 <Julien00859@gmail.com>
2015-11-18 00:11:25 +0100

    Hardtab

--- js/client.js
+++ js/client.js
@@ -1,124 +1,126 @@
-var log
-function main() {
-    document.getElementById("cursor").style.borderColor = "white";
-    setInterval(blinkCursor, 500);
+var log;
+var socket;
 
-    /*var socket = io.connect('http://Julien00859.no-ip.org:8080');
-    socket.on("write", write(ypos, xpos, text));
-    socket.on("erase", erase(ypos, xpos, length));
-    socket.on("cursor", cursor(ypos, xpos));
-    socket.on("setCoder", setCoder(coder, link));
-    socket.on("setProject", setProject(project, link));
-    socket.on("setDesc", setDesc(desc));
-    socket.on("setFile", setFile(file));
-    */
-    log = new Julien.log();
-    log.disable();
-    document.getElementById("code").style.width = "calc(100% - (2 *" + document.getElementById("line").offsetWidth + "px))";
+function main() {
+		document.getElementById("cursor").style.borderColor = "white";
+		setInterval(blinkCursor, 500);
+
+		socket = io.connect();
+		socket.on("write", write(ypos, xpos, text));
+		socket.on("erase", erase(ypos, xpos, length));
+		socket.on("cursor", cursor(ypos, xpos));
+		socket.on("setCoder", setCoder(coder, link));
+		socket.on("setProject", setProject(project, link));
+		socket.on("setDesc", setDesc(desc));
+		socket.on("setFile", setFile(file));
+
+		log = new Julien.log();
+		log.disable();
+		document.getElementById("code").style.width = "calc(100% - (2 *" + document.getElementById("line").offsetWidth + "px))";
 }
 
 function blinkCursor() {
-    var cursor = document.getElementById("cursor")
-    var activeLine = document.getElementById("code").getElementsByClassName("active")[0]
-    if (cursor) {
-        if (cursor.style.borderColor == "white") {
-            cursor.style.borderColor = "black";
-        } else {
-            cursor.style.borderColor = "white";
-        }
-    }
+		var cursor = document.getElementById("cursor")
+		var activeLine = document.getElementById("code").getElementsByClassName("active")[0]
+		if (cursor) {
+				if (cursor.style.borderColor == "white") {
+						cursor.style.borderColor = "black";
+				} else {
+						cursor.style.borderColor = "white";
+				}
+		}
 }
 
 function write(ypos, xpos, text) {
-    var lines = document.getElementById("code").getElementsByTagName("p");
-    var line = lines[ypos].textContent;
-    if (text.indexOf("\n") == -1) {
-        lines[ypos].textContent = line.substring(0, xpos) + text + line.substring(xpos, line.length);
-    } else {
-        line = line.substring(0, xpos) + text + line.substring(xpos, line.length);
-        line = line.split("\n");
-        lines[ypos].textContent = line[0];
-        for (var n=1; n < line.length; n++) {
-            var DOMCode = document.createElement("p");
-            var DOMLine = document.createElement("p");
-            var DOMTextCode = document.createTextNode(line[n]);
-            var DOMTextLine = document.createTextNode((ypos + n).toString());
-            DOMCode.appendChild(DOMTextCode);
-            DOMLine.appendChild(DOMTextLine);
-            document.getElementById("code").insertBefore(DOMCode, document.getElementById("code").getElementsByTagName("p")[ypos+n]);
-            document.getElementById("line").insertBefore(DOMLine, document.getElementById("line").getElementsByTagName("p")[ypos+n]);
-        }
-    }
-    document.getElementById("code").style.width = "calc(100% - (2 *" + document.getElementById("line").offsetWidth + "px))";
-    for (var n=0, lines=document.getElementById("line").getElementsByTagName("p"), code=document.getElementById("code").getElementsByTagName("p"); n<lines.length; n++) {
-        lines[n].innerHTML = (n+1).toString().fill("&nbsp;", (lines.length-1).toString().length);
-        lines[n].style.height = code[n].offsetHeight + "px";
-    }
+		var lines = document.getElementById("code").getElementsByTagName("p");
+		var line = lines[ypos].textContent;
+		if (text.indexOf("\n") == -1) {
+				lines[ypos].textContent = line.substring(0, xpos) + text + line.substring(xpos, line.length);
+		} else {
+				line = line.substring(0, xpos) + text + line.substring(xpos, line.length);
+				line = line.split("\n");
+				lines[ypos].textContent = line[0];
+				for (var n=1; n < line.length; n++) {
+						var DOMCode = document.createElement("p");
+						var DOMLine = document.createElement("p");
+						var DOMTextCode = document.createTextNode(line[n]);
+						var DOMTextLine = document.createTextNode((ypos + n).toString());
+						DOMCode.appendChild(DOMTextCode);
+						DOMLine.appendChild(DOMTextLine);
+						document.getElementById("code").insertBefore(DOMCode, document.getElementById("code").getElementsByTagName("p")[ypos+n]);
+						document.getElementById("line").insertBefore(DOMLine, document.getElementById("line").getElementsByTagName("p")[ypos+n]);
+				}
+		}
+		document.getElementById("code").style.width = "calc(100% - (2 *" + document.getElementById("line").offsetWidth + "px))";
+		for (var n=0, lines=document.getElementById("line").getElementsByTagName("p"), code=document.getElementById("code").getElementsByTagName("p"); n<lines.length; n++) {
+				lines[n].innerHTML = (n+1).toString().fill("&nbsp;", (lines.length-1).toString().length);
+				lines[n].style.height = code[n].offsetHeight + "px";
+		}
 }
 
 function erase(ypos, xpos, length) {
-    var code = document.getElementById("code").getElementsByTagName("p");
-    var line = document.getElementById("line").getElementsByTagName("p");
-    while(xpos + length > code[ypos].textContent.length) {
-        code[ypos].textContent = code[ypos].textContent + code[ypos+1].textContent;
-        code[ypos+1].parentNode.removeChild(code[ypos+1]);
-        line[ypos+1].parentNode.removeChild(line[ypos+1]);
-    }
-    code[ypos].textContent = code[ypos].textContent.substring(0, xpos) + code[ypos].textContent.substring(xpos+length, code[ypos].textContent.length);
+		var code = document.getElementById("code").getElementsByTagName("p");
+		var line = document.getElementById("line").getElementsByTagName("p");
+		while(xpos + length > code[ypos].textContent.length) {
+				code[ypos].textContent = code[ypos].textContent + code[ypos+1].textContent;
+				code[ypos+1].parentNode.removeChild(code[ypos+1]);
+				line[ypos+1].parentNode.removeChild(line[ypos+1]);
+		}
+		code[ypos].textContent = code[ypos].textContent.substring(0, xpos) + code[ypos].textContent.substring(xpos+length, code[ypos].textContent.length);
 
-    for (var n=0, lines=document.getElementById("line").getElementsByTagName("p"), code=document.getElementById("code").getElementsByTagName("p"); n<code.length; n++) {
-        lines[n].innerHTML = (n+1).toString().fill("&nbsp;", (lines.length-1).toString().length);
-        lines[n].style.height = code[n].offsetHeight + "px";
-    }
+		for (var n=0, lines=document.getElementById("line").getElementsByTagName("p"), code=document.getElementById("code").getElementsByTagName("p"); n<code.length; n++) {
+				lines[n].innerHTML = (n+1).toString().fill("&nbsp;", (lines.length-1).toString().length);
+				lines[n].style.height = code[n].offsetHeight + "px";
+		}
 }
 
 function eraseLine(ypos) {
-    document.getElementById("code").removeChild(document.getElementById("code").getElementsByTagName("p")[ypos]);
-    document.getElementById("line").removeChild(document.getElementById("line").getElementsByTagName("p")[0]);
-    for (var n=0, lines=document.getElementById("line").getElementsByTagName("p"), code=document.getElementById("code").getElementsByTagName("p"); n<code.length; n++) {
-        lines[n].innerHTML = (n+1).toString().fill("&nbsp;", (lines.length-1).toString().length);
-        lines[n].style.height = code[n].offsetHeight + "px";
-    }
+		document.getElementById("code").removeChild(document.getElementById("code").getElementsByTagName("p")[ypos]);
+		document.getElementById("line").removeChild(document.getElementById("line").getElementsByTagName("p")[0]);
+		for (var n=0, lines=document.getElementById("line").getElementsByTagName("p"), code=document.getElementById("code").getElementsByTagName("p"); n<code.length; n++) {
+				lines[n].innerHTML = (n+1).toString().fill("&nbsp;", (lines.length-1).toString().length);
+				lines[n].style.height = code[n].offsetHeight + "px";
+		}
 }
 
 function cursor(ypos, xpos) {
-    document.getElementById("cursor").parentNode.removeChild(document.getElementById("cursor"));
-    var code = document.getElementById("code").getElementsByTagName("p");
-    var line = document.getElementById("line").getElementsByTagName("p");
+		document.getElementById("cursor").parentNode.removeChild(document.getElementById("cursor"));
+		var code = document.getElementById("code").getElementsByTagName("p");
+		var line = document.getElementById("line").getElementsByTagName("p");
 
-    for(var i = 0; i < code.length; i++) {
-        if (code[i].hasAttribute("class") && code[i].className === "active") {
-            code[i].className = "";
-            line[i].className = "";
-            break;
-        }
-    }
+		for(var i = 0; i < code.length; i++) {
+				if (code[i].hasAttribute("class") && code[i].className === "active") {
+						code[i].className = "";
+						line[i].className = "";
+						break;
+				}
+		}
 
-    code[ypos].innerHTML = code[ypos].textContent.substring(0, xpos) + "<span id=cursor></span>" + code[ypos].textContent.substring(xpos, code[ypos].length);
-    code[ypos].setAttribute("class", "active");
-    line[ypos].setAttribute("class", "active");
+		code[ypos].innerHTML = code[ypos].textContent.substring(0, xpos) + "<span id=cursor></span>" + code[ypos].textContent.substring(xpos, code[ypos].length);
+		code[ypos].setAttribute("class", "active");
+		line[ypos].setAttribute("class", "active");
 }
 
 function setCoder(coder, link) {
-    var coderDOM = document.getElementById("coder");
-    coderDOM.setAttribute("a", link);
-    coderDOM.textContent = coder;
+		var coderDOM = document.getElementById("coder");
+		coderDOM.setAttribute("a", link);
+		coderDOM.textContent = coder;
 }
 
 function setProject(project, link) {
-    var projectDOM = document.getElementById("project");
-    projectDOM.setAttribute("a", link);
-    projectDOM.textContent = project;
+		var projectDOM = document.getElementById("project");
+		projectDOM.setAttribute("a", link);
+		projectDOM.textContent = project;
 }
 
 function setDesc(desc) {
-    document.getElementById("desc").textContent = desc;
+		document.getElementById("desc").textContent = desc;
 }
 
 function setFile(file) {
-    document.getElementById("file").textContent = file;
+		document.getElementById("file").textContent = file;
 }
 
 function show(line) {
-    console.log(document.getElementById("code").getElementsByTagName("p")[line].innerHTML)
+		console.log(document.getElementById("code").getElementsByTagName("p")[line].innerHTML)
 }
--- server.js
+++ server.js
@@ -3,41 +3,41 @@ var io = require("socket.io")(http);
 var fs = require("fs"); // File System
 
 http.on("request", function(req, res) {
-  // Si on peut servir le fichier
-  if (["/client.html", "/host.html", "/js/client.js", "/js/host.js", "/js/utils.js", "/socket.io/socket.io.js", "/css/style.css"].indexOf(req.url) !== -1) {
-    res.statusCode = 200;
+	// Si on peut servir le fichier
+	if (["/client.html", "/host.html", "/js/client.js", "/js/host.js", "/js/utils.js", "/socket.io/socket.io.js", "/css/style.css"].indexOf(req.url) !== -1) {
+		res.statusCode = 200;
 
-    // On selectionne le header à mettre
-    switch (req.url) {
-      case "/client.html":
-      case "/host.html":
-        res.setHeader("Content-Type", "text/html");
-        break;
-      case "/js/clients.js":
-      case "/js/host.js":
-      case "/js/utils.js":
-        res.setHeader("Content-Type", "text/javascript");
-        break;
-      case "/css/style.css":
-        res.setHeader("Content-Type", "text/css");
-        break;
-    }
-    // Puis on envoie le fichier
-    fs.readFile(__dirname + req.url, function(err, data){
-      if (!err) res.end(data);
-    })
-  } else {
+		// On selectionne le header à mettre
+		switch (req.url) {
+			case "/client.html":
+			case "/host.html":
+				res.setHeader("Content-Type", "text/html");
+				break;
+			case "/js/clients.js":
+			case "/js/host.js":
+			case "/js/utils.js":
+				res.setHeader("Content-Type", "text/javascript");
+				break;
+			case "/css/style.css":
+				res.setHeader("Content-Type", "text/css");
+				break;
+		}
+		// Puis on envoie le fichier
+		fs.readFile(__dirname + req.url, function(err, data){
+			if (!err) res.end(data);
+		})
+	} else {
 
-    // Une erreur 404
-    console.log("404: " + req.url);
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/raw");
-    res.end();
-  }
+		// Une erreur 404
+		console.log("404: " + req.url);
+		res.statusCode = 404;
+		res.setHeader("Content-Type", "text/raw");
+		res.end();
+	}
 }).listen(59600, function() {
-  console.log("listening on *:59600");
+	console.log("listening on *:59600");
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('a user connected');
 });
