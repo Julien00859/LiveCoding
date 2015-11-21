8589cfa55fed34cb77e6066c0c8d1a87a4a26dec
Julien00859 <Julien00859@gmail.com>
2015-11-21 18:16:37 +0100

    Ajout de la gestion des flèches directionnel, ajout de commentaires, modification de la fonction d'activation pour positionner le curseur en plus d'activer la ligne, restructuration du code, correction d'une erreur d'indentation

--- js/host.js
+++ js/host.js
@@ -1,13 +1,45 @@
-var log;
 var socket;
 
 function main() {
 	newLine();
-	log = new Julien.log();
 	socket = io.connect();
 	socket.emit("clientType", "host");
 }
 
+// Crée une nouvelle ligne de code
+function newLine(ypos) {
+	var dLine = document.getElementById("line");
+	var lines = dLine.getElementsByTagName("p");
+
+	if (ypos === undefined) ypos = lines.length; // Si la ligne n'a pas été donnée, on ajoute la nouvelle ligne à la fin
+
+	// Ajoute une ligne de code avec la gestion événementielle et les attributs
+	var dCode = document.getElementById("code");
+	var lCode = dCode.getElementsByTagName("p");
+	var pCode = document.createElement("p");
+	pCode.setAttribute("contenteditable", "true");
+	pCode.addEventListener("focus", function(e) {
+		if (e.isTrusted) activate(document.getElementById("code").getElementsByTagName("p").toArray().indexOf(e.target));
+	});
+	pCode.addEventListener("blur", function(e) {
+		if (e.isTrusted) activate(null, null);
+	});
+	pCode.addEventListener("keypress", keyPressed);
+
+	// Positionne la ligne de code
+	if (ypos < lCode.length) {
+		dCode.insertBefore(pCode, lCode[ypos]);
+	} else {
+		dCode.appendChild(pCode);
+	}
+
+	dLine.appendChild(document.createElement("p"));
+	updateLine();
+	resetWidth();
+	return ypos;
+}
+
+// Supprime une ligne de code donnée
 function removeLine(ypos) {
 	var dLine = document.getElementById("line");
 	var lines = dLine.getElementsByTagName("p");
@@ -16,53 +48,32 @@ function removeLine(ypos) {
 	if (ypos >= 0 && ypos < lines.length) {
 		dLine.removeChild(lines[ypos]);
 		dCode.removeChild(lCode[ypos]);
-		updateLine();
+		updateLine(); // Met à jour les numéro de ligne et leur taille
+		resetWidth(); // Met à jour la taille de la zone code
 	}
 }
 
-function newLine(ypos) {
-	var dLine = document.getElementById("line");
-	var lines = dLine.getElementsByTagName("p");
-
-	if (ypos === undefined) {
-		ypos = lines.length;
-	}
-
-	var pLine = document.createElement("p");
-	pLine.textContent = lines.length + 1;
-	dLine.appendChild(pLine);
-
-	var dCode = document.getElementById("code");
-	var lCode = dCode.getElementsByTagName("p");
-	var pCode = document.createElement("p");
-	pCode.setAttribute("contenteditable", "true");
-	pCode.addEventListener("focus", function(e) {
-		activate(document.getElementById("code").getElementsByTagName("p").toArray().indexOf(e.target));
-	});
-	pCode.addEventListener("blur", function() {
-		activate(-1);
-	});
-	pCode.addEventListener("keydown", keyPressed);
-
-	if (ypos < lCode.length) {
-		dCode.insertBefore(pCode, lCode[ypos]);
-	} else {
-		dCode.appendChild(pCode);
-	}
-	updateLine();
-	resetWidth();
-	return ypos;
-}
-
-function activate(ypos) {
+// Active la ligne donnée et place le curseur à la position donnée
+function activate(ypos, xpos) {
 	var lCode = document.getElementById("code").getElementsByTagName("p");
 	var lines = document.getElementById("line").getElementsByTagName("p");
 	for (var i = 0; i < lines.length; i++) {
 		if (i === ypos) {
+			//Positionne le curseur
+			if(xpos) {
+				var sel = window.getSelection();
+				var range = document.createRange();
+				range.setStart(lCode[i], xpos);
+				sel.removeAllRanges();
+				sel.addRange(range);
+			}
+			// Active la ligne
 			lCode[i].className = "active";
 			lines[i].className = "active";
-			lCode[i].focus();
+			lCode[i].dispatchEvent(new Event("focus", {"bubbles":true}));
+
 		} else if (lCode[i].className === "active" || lines[i].className === "active") {
+			// Désactive la ligne
 			lCode[i].removeAttribute("class");
 			lines[i].removeAttribute("class");
 		}
@@ -70,42 +81,76 @@ function activate(ypos) {
 	return lCode[ypos];
 }
 
+// Recalcule la longueure de la zone code en fonction de la zone ligne pour que ligne + code ~= 100%
 function resetWidth() {
 	document.getElementById("code").style.width = "calc(100% - " + document.getElementById("line").offsetWidth + "px - 10px)";
 }
 
+// Met à jour le numéro des lignes
 function updateLine() {
-		for (var i = 0, lines = document.getElementById("line").getElementsByTagName("p"); i < lines.length; i++) {
-			lines[i].innerHTML = (i+1).toString().fill(" ", lines[lines.length - 1].textContent.length).replaceAll(" ", "&nbsp;");
-		}
+	for (var i = 0, lines = document.getElementById("line").getElementsByTagName("p"); i < lines.length; i++) {
+		lines[i].innerHTML = (i+1).toString().fill(" ", lines[lines.length - 1].textContent.length).replaceAll(" ", "&nbsp;");
+	}
 }
 
+// Gestion de la touche qui vient d'être tapée dans la zone de texte.
 function keyPressed(e) {
 	var lines = document.getElementById("code").getElementsByTagName("p")
 	var ypos = lines.toArray().indexOf(e.target);
 	var xpos = getSelection().anchorOffset;
-	if (e.keyCode === 13 || e.keyCode === 10) { // new line
-		activate(newLine(ypos + 1));
+	// console.log("ypos: " + ypos + ", xpos: " + xpos + ", char: " + e.key + " (" + e.code + ")");
+	if (e.key === "Enter") {
+		activate(newLine(ypos + 1), 1);
 		lines[ypos+1].textContent = lines[ypos].textContent.slice(xpos);
-		lines[ypos].textContent = lines[ypos].textContent.slice(-xpos);
-		e.stopPropagation();
+		lines[ypos].textContent = lines[ypos].textContent.slice(0, xpos);
 		e.preventDefault();
 		socket.emit("write", ypos, xpos, "\n")
-	} else if (e.keyCode === 8 || e.keyCode === 127) { // backspace
+	} else if (e.code === "Backspace" || e.code === "Delete") {
 		if (e.target.textContent.length) socket.emit("erase", ypos, e.keyCode === 8 ? xpos - 1 : xpos, 1);
 		else if (lines.length > 1){
 			socket.emit("eraseLine", ypos);
-			e.target.parentNode.getElementsByTagName("p")[ypos-1].focus();
+			activate(ypos-1, lines[ypos-1].textContent.length);
 			document.getElementById("code").removeChild(e.target);
 			document.getElementById("line").removeChild(document.getElementById("line").getElementsByTagName("p")[ypos]);
 		}
-	} else if (e.keyCode === 9) { // tabulation
+	} else if (e.code === "Tab") {
 		socket.emit("write", ypos, xpos, "\t")
-		e.stopPropagation();
 		e.preventDefault();
+	} else if (e.code.slice(0,5) == "Arrow") {
+		switch (e.code.slice(5)) {
+			case "Left":
+				if(xpos === 0 && ypos > 0) {
+					e.preventDefault;
+					activate(ypos-1, lines[ypos-1].textContent.length);
+					socket.emit("cursor", ypos-1, lines[ypos-1].textContent.length);
+				} else if (xpos > 0) {
+					socket.emit("cursor", ypos, xpos-1)
+				}
+				break;
+			case "Right":
+				if(xpos === lines[ypos].textContent.length && ypos < lines.length - 1) {
+					e.preventDefault;
+					activate(ypos+1, 0);
+					socket.emit("cursor", ypos+1, 0);
+				} else if (xpos < lines[ypos].textContent.length) {
+					socket.emit("cursor", ypos, xpos+1)
+				}
+				break;
+			case "Up":
+				if (ypos > 0) {
+					socket.emit("cursor", ypos-1, xpos);
+					activate(ypos-1, xpos <= lines[ypos-1].textContent.length ? xpos : lines[ypos-1].textContent.length);
+				}
+				break;
+			case "Down":
+				if (ypos < lines.length - 1) {
+					activate(ypos+1, xpos <= lines[ypos+1].textContent.length ? xpos : lines[ypos+1].textContent.length);
+					socket.emit("cursor", ypos+1, xpos);
+				}
+				break;
+		}
 	} else { // any other key
 		socket.emit("write", ypos, xpos, e.key);
-		console.log(e.code + " : " + e.key);
+		socket.emit("cursor", ypos, xpos+1);
 	}
-	socket.emit("cursor", ypos, xpos+1);
 }
